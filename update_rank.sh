acffcdf722457bec783c64c8dd914c605f182e14
David <dsdavidschwarz@gmail.com>
2025-10-29 22:29:57 +0000

    test bbr integration

--- update_rank.sh
+++ update_rank.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Copy battle reports to git repo as a backup
-cp -r /srv/BattleReports /root/nebulous-ranking/docs/
+cp -r /srv/steam/.steam/steam/steamapps/common/NEBULOUS\ Dedicated\ Server/Saves/SkirmishReports/*.xml /root/nebulous-ranking/docs/BattleReports/
+cp -r /srv/steam/.steam/steam/steamapps/common/NEBULOUS\ Dedicated\ Server/Saves/SkirmishReports/*.bbr /root/nebulous-ranking/docs/replays/
 
 # Activate virtual environment and run script
 source /root/nebulous-ranking/.venv/bin/activate
